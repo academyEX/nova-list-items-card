df14bfc6586b1d1f3ec68167dbf010af2ae50295
Andy <kiwikefei@gmail.com>
2022-10-31 10:16:06 +1300

    init commit

---
+++ resources/js/card.js
@@ -0,0 +1,3 @@
+Nova.booting((Vue, router, store) => {
+  Vue.component('nova-list-items-card', require('./components/Card'))
+})
---
+++ src/NovaListItemsCard.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pixelfusion\NovaListItemsCard;
+
+use Laravel\Nova\Card;
+
+class NovaListItemsCard extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = 'full';
+
+    public array $items = [];
+
+    /**
+     * Get the component name for the element.
+     *
+     * @return string
+     */
+    public function component(): string
+    {
+        return 'nova-list-items-card';
+    }
+    public function setItems(array $items): static
+    {
+        $this->items = $items;
+        return $this;
+    }
+
+    public function render(): NovaListItemsCard
+    {
+        return $this->withMeta(['items' => $this->items]);
+    }
+
+}
