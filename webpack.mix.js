ca7df93773552f7cd25e8be408a8200f4c87cb12
andyaex <kiwikefei@gmail.com>
2024-01-25 08:54:07 +1300

    feat: support nova 4 and prod build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 let mix = require('laravel-mix')
+require('./nova.mix')
 
 mix
   .setPublicPath('dist')
+  .vue({ version: 3 })
   .js('resources/js/card.js', 'js')
   .sass('resources/sass/card.scss', 'css')
+  .nova('academyex/nova-list-items-card')
